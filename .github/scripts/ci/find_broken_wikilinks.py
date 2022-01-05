29ce1ccbd6cbcb3b44204d7822744908e6d252a9
Walavouchey <36758269+Walavouchey@users.noreply.github.com>
2022-01-05 04:15:32 +0100

    update script to fix comment detector and other things

--- .github/scripts/ci/find_broken_wikilinks.py
+++ .github/scripts/ci/find_broken_wikilinks.py
@@ -3,7 +3,40 @@ import os
 import sys
 import typing
 
+
 Redirects = typing.Dict[str, typing.Tuple[str, int]]
+
+
+class Comment(typing.NamedTuple):
+    """
+    An HTML comment in a line.
+
+    These mark regions where parsed content should be discarded.
+
+    Example:
+        <!-- this is a comment -->
+        ^ start                  ^ end
+
+    Since an article is read and errors are printed line-by-line
+    (to protect against unexpected crashes), multiline comments are
+    expressed by setting the start and/or end values to -1, indicating
+    continuation of a comment from a previous line or to subsequent
+    lines respectively.
+
+    Examples:
+
+        A multiline comment continuing from a previous line -->
+        (start = -1)                                          ^ end
+
+        <!-- A multiline comment continuing off to subsequent lines
+        ^ start                                          (end = -1)
+
+        A whole line marked as part of a multiline comment
+        (start = -1)                          (end = -1)
+    """
+
+    start: int
+    end: int
 
 class Link(typing.NamedTuple):
     """
@@ -24,10 +57,6 @@ class Link(typing.NamedTuple):
     - extra: ' "Player is away from keyboard"'
     """
 
-    title: str
-    location: str
-    extra: str
-
     # Link position within the line. Example:
     #   See also: [Difficulty names](/wiki/Beatmap/Difficulty#naming-conventions)
     #             ^ link_start                                                  ^ link_end
@@ -36,17 +65,22 @@ class Link(typing.NamedTuple):
 
     # Sections of a link. Example:
     #    ![Player is AFK](img/chat-console-afk.png "Player is away from keyboard")
+    #      ^ - title - ^
     #                     ^ ----- location ----- ^
     #                                             ^ ---------- extra ---------- ^
     #                     ^ --------------------- content --------------------- ^
     #     ^ ------------------ full_link / full_coloured_link ------------------ ^
+    title: str
+    location: str
+    extra: str
+
     @property
     def content(self):
         return self.location + self.extra
 
     @property
     def full_link(self):
-        return f"[{self.title}]{self.content}"
+        return f"[{self.title}]({self.content})"
 
     @property
     def full_coloured_link(self):
@@ -127,8 +161,55 @@ def check_link(redirects: Redirects, directory: str, link: str) -> typing.Tuple[
         return (True, "")
 
 
-def is_in_comment(s, start, end):
-    return s.rfind("<!--", 0, start) != -1 and s.find("-->", end, -1)
+def find_comments(line: str, in_multiline: bool) -> typing.List[Comment]:
+    comments = []
+    index = 0
+    start = None
+
+    while True:
+        # don't start a comment if already in one
+        if not in_multiline:
+            start = line.find("<!--", index)
+            if start == -1:
+                # no more comments
+                return comments
+
+        end = line.find("-->", start or 0)
+
+        if end != -1:
+            # found the end of a comment
+            if in_multiline:
+                # end of a multiline comment
+                comments.append(Comment(start=-1, end=end + 2))
+                in_multiline = False
+            else:
+                # whole inline comment
+                comments.append(Comment(start=start, end=end + 2))
+            index = end + 3
+            continue
+        elif start is None:
+            # no comment start or end; the whole line is part of a comment
+            comments.append(Comment(start=-1, end=-1))
+            return comments
+        else:
+            # unmatched comment start: continuing to subsequent lines
+            comments.append(Comment(start=start, end=-1))
+            return comments
+
+
+def is_in_comment(index: int, comments: typing.List[Comment]) -> bool:
+    for comment in comments:
+        left_bound = comment.start
+        right_bound = comment.end
+        if comment.start == -1:
+            left_bound = float('-inf')
+        if comment.end == -1:
+            right_bound = float('inf')
+
+        if index >= left_bound and index <= right_bound:
+            return True
+
+    return False
 
 
 def find_link(s: str, index=0) -> typing.Optional[Link]:
@@ -143,7 +224,7 @@ def find_link(s: str, index=0) -> typing.Optional[Link]:
     for i, c in enumerate(s[index:]):
         i += index
         if not found_brackets and c == '[':
-            if not start:
+            if not started:
                 start = i
                 started = True
             square_bracket_level += 1
@@ -166,8 +247,6 @@ def find_link(s: str, index=0) -> typing.Optional[Link]:
             parenthesis_level -= 1
             if parenthesis_level == 0:
                 end = i
-                if is_in_comment(s, start, end):
-                    return None
                 if extra is None:
                     extra = end
 
@@ -211,18 +290,34 @@ def print_clean():
 def parse_args(args):
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--target", nargs='*', help="paths to the articles you want to check")
+    parser.add_argument("-a", "--all", action='store_true', help="check all articles")
     return parser.parse_args(args)
 
+def file_iterator(roots: list):
+    for item in roots:
+        if os.path.isdir(item):
+            for root, _, filenames in os.walk(item):
+                for f in filenames:
+                    filepath = os.path.join(root, f)
+                    yield filepath
+        elif os.path.isfile(item):
+            yield item
 
 def main():
     args = parse_args(sys.argv[1:])
-    if not args.target:
+    if not args.target and not args.all:
         print("Notice: No articles to check.")
         sys.exit(0)
 
+    filenames = []
+    if args.all:
+        filenames = file_iterator(["wiki", "news"])
+    else:
+        filenames = args.target
+
     redirects = load_redirects("wiki/redirect.yaml")
     exit_code = 0
-    for filename in args.target:
+    for filename in filenames:
         filename = filename.replace('\\', '/')
         if filename.startswith("./"):
             filename = filename[2:]
@@ -235,8 +330,16 @@ def main():
             continue
 
         with open(filename, 'r', encoding='utf-8') as fd:
+            in_multiline = False
             for linenumber, line in enumerate(fd, start=1):
+                comments = find_comments(line, in_multiline)
+                if comments:
+                    in_multiline = comments[-1].end == -1
+
                 for match in find_links(line):
+                    if is_in_comment(match.link_start, comments):
+                        continue
+
                     if match.content == "/wiki/Sitemap":
                         continue
                     success, note = check_link(redirects, directory(filename), match.location)
@@ -246,6 +349,7 @@ def main():
                     if exit_code == 0:
                         print_error()
                     exit_code = 1
+
                     print(f"{yellow(filename)}:{linenumber}:{match.link_start + 1}: {red(match.location)}")
                     if note:
                         print(note)
